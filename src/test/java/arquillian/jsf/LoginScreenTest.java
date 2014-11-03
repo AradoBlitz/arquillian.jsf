08feccdef19e2c090a65f429720039036dbb8125
AradoBlitz <schlachtnavaho@gmail.com>
2014-11-04 00:33:58 +0200

    Implemented userName and password fields in login form.

--- src/test/java/arquillian/jsf/LoginScreenTest.java
+++ src/test/java/arquillian/jsf/LoginScreenTest.java
@@ -2,6 +2,7 @@ package arquillian.jsf;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.net.URL;
 
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -24,6 +25,7 @@ public class LoginScreenTest {
 	@Deployment(testable=false)
 	public static WebArchive create(){
 		return ShrinkWrap.create(WebArchive.class)
+				.addAsWebResource(new File("src/main/webapp","login.html"))
 				.addAsWebInfResource(new StringAsset("<faces-config version=\"2.0\"/>"), "faces-config.xml");
 	}
 	
@@ -32,7 +34,7 @@ public class LoginScreenTest {
 
 	private WebElement loginButton;
 	
-
+	@FindBy
 	private WebElement userName;
 
 	@FindBy
@@ -50,10 +52,13 @@ public class LoginScreenTest {
 		//Browser nullpointer - add Arquillian tests runner.Add arquillian dependency.
 		//deploy package.
 		browser.get(deploymentUrl.toExternalForm()+"login.html");
+		
 		assertNotNull("UserName field should be provided.", userName);
-		/*userName.sendKeys("demo");
+		userName.sendKeys("demo");
+		
+		assertNotNull("Password field should be provided.", password);
 		password.sendKeys("demo");
-		Graphene.guardHttp(loginButton).click();
+		/*Graphene.guardHttp(loginButton).click();
 		assertEquals("Welcom text should be present.","Welcome",facesMessage.getText().trim());*/
 	}
 }
